2df66fc50bdbcc902a4f50c6d0c583590e5766dd
Jeremy <jneveu@lal.in2p3.fr>
2018-03-25 18:09:45 +0200

    move filters

--- optics.py
+++ optics.py
@@ -6,6 +6,17 @@ from tools import *
 from scipy.signal import argrelextrema
 from astropy.table import Table
 
+# H-alpha filter
+HALPHA_CENTER = 655.9e-6 # center of the filter in mm
+HALPHA_WIDTH = 6.4e-6 # width of the filter in mm
+
+# Other filters
+FGB37 = {'label':'FGB37','min':300,'max':800}
+RG715 = {'label':'RG715','min':690,'max':1100}
+HALPHA_FILTER = {'label':'Halfa','min':HALPHA_CENTER-2*HALPHA_WIDTH,'max':HALPHA_CENTER+2*HALPHA_WIDTH}
+ZGUNN = {'label':'Z-Gunn','min':800,'max':1100}
+FILTERS = [RG715,FGB37,HALPHA_FILTER,ZGUNN]
+
 class Line():
 
     def __init__(self,wavelength,label,atmospheric=False,emission=False,label_pos=[0.007,0.02],width_bounds=[1,10]):
