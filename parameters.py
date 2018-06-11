2d8c5a7218b1674da4be206abfbb69c9ffd98b5f
Jérémy Neveu <jneveu@lal.in2p3.fr>
2018-06-11 17:44:44 +0200

    Lots of clean-up: remove unused varaibles, optimise imports, follow PEP8 recommandations

--- parameters.py
+++ parameters.py
@@ -1,53 +1,55 @@
-import os, sys
-mypath = os.path.dirname(__file__)
-import coloredlogs, logging
+import coloredlogs
+import logging
 import numpy as np
 import astropy.units as units
 from astropy import constants as const
-
+import os
+mypath = os.path.dirname(__file__)
 
 # Paths
-HOLO_DIR = os.path.join(mypath,"dispersers/")
+HOLO_DIR = os.path.join(mypath, "dispersers/")
 
 # CCD characteristics
-IMSIZE = 2048 # size of the image in pixel
-PIXEL2MM = 24e-3 # pixel size in mm
-PIXEL2ARCSEC = 0.401 # pixel size in arcsec
-ARCSEC2RADIANS = np.pi/(180.*3600.) # conversion factor from arcsec to radians
-MAXADU = 60000 # approximate maximum ADU output of the CCD
-GAIN = 3. # electronic gain : elec/ADU
+IMSIZE = 2048  # size of the image in pixel
+PIXEL2MM = 24e-3  # pixel size in mm
+PIXEL2ARCSEC = 0.401  # pixel size in arcsec
+ARCSEC2RADIANS = np.pi / (180. * 3600.)  # conversion factor from arcsec to radians
+MAXADU = 60000  # approximate maximum ADU output of the CCD
+GAIN = 3.  # electronic gain : elec/ADU
 
 # Observatory characteristics
-OBS_LATITUDE = '-30 10 07.90' # CTIO latitude
-OBS_DIAMETER = 0.9*units.m    # Diameter of the telescope
-OBS_SURFACE = np.pi*OBS_DIAMETER**2/4. # Surface of telescope
+OBS_LATITUDE = '-30 10 07.90'  # CTIO latitude
+OBS_DIAMETER = 0.9 * units.m  # Diameter of the telescope
+OBS_SURFACE = np.pi * OBS_DIAMETER ** 2 / 4.  # Surface of telescope
+EPOCH = "J2000.0"
 
 # Conversion factor
 # Units of SEDs in flam (erg/s/cm2/nm) :
-SED_UNIT = 1*units.erg/units.s/(units.cm)**2/(units.nanometer)  
-TIME_UNIT=1*units.s               # flux for 1 second
-hc=const.h*const.c     # h.c product of fontamental constants c and h 
-wl_dwl_unit=(units.nanometer)**2  # lambda.dlambda  in wavelength in nm
-g_disperser_ronchi=0.2            # theoretical gain for order+1 : 20%
-FLAM_TO_ADURATE=((OBS_SURFACE*SED_UNIT*TIME_UNIT*wl_dwl_unit/hc/GAIN*g_disperser_ronchi).decompose()).value
+SED_UNIT = 1 * units.erg / units.s / units.cm ** 2 / units.nanometer
+TIME_UNIT = 1 * units.s  # flux for 1 second
+hc = const.h * const.c  # h.c product of fontamental constants c and h
+wl_dwl_unit = units.nanometer ** 2  # lambda.dlambda  in wavelength in nm
+g_disperser_ronchi = 0.2  # theoretical gain for order+1 : 20%
+FLAM_TO_ADURATE = (
+    (OBS_SURFACE * SED_UNIT * TIME_UNIT * wl_dwl_unit / hc / GAIN * g_disperser_ronchi).decompose()).value
 
 # Search windows in images
-XWINDOW = 100 # window x size to search for the targetted object 
-YWINDOW = 100 # window y size to search for the targetted object
-XWINDOW_ROT = 50 # window x size to search for the targetted object 
-YWINDOW_ROT = 50 # window y size to search for the targetted object
+XWINDOW = 100  # window x size to search for the targetted object
+YWINDOW = 100  # window y size to search for the targetted object
+XWINDOW_ROT = 50  # window x size to search for the targetted object
+YWINDOW_ROT = 50  # window y size to search for the targetted object
 
 # Rotation parameters
-ROT_PREFILTER = True # must be set to true, otherwise create residuals and correlated noise
-ROT_ORDER = 5 # must be above 3
+ROT_PREFILTER = True  # must be set to true, otherwise create residuals and correlated noise
+ROT_ORDER = 5  # must be above 3
 
 # Range for spectrum
-LAMBDA_MIN = 350 # minimum wavelength for spectrum extraction (in nm)
-LAMBDA_MAX = 1100 # maxnimum wavelength for spectrum extraction (in nm)
+LAMBDA_MIN = 350  # minimum wavelength for spectrum extraction (in nm)
+LAMBDA_MAX = 1100  # maxnimum wavelength for spectrum extraction (in nm)
 
 # Detection line algorithm
-BGD_ORDER = 3 # order of the background polynome to fit
-BGD_NPARAMS = BGD_ORDER + 1 # number of unknown parameters for background
+BGD_ORDER = 3  # order of the background polynome to fit
+BGD_NPARAMS = BGD_ORDER + 1  # number of unknown parameters for background
 
 # Plotting
 PAPER = False
@@ -58,18 +60,19 @@ SAVE = False
 # Verbosity
 VERBOSE = False
 DEBUG = False
-MY_FORMAT = "%(asctime)-20s %(name)-10s %(funcName)-20s %(levelname)-6s %(message)s"  
+MY_FORMAT = "%(asctime)-20s %(name)-10s %(funcName)-20s %(levelname)-6s %(message)s"
 logging.basicConfig(format=MY_FORMAT, level=logging.WARNING)
+
+
 def set_logger(logger):
     my_logger = logging.getLogger(logger)
     if VERBOSE > 0:
         my_logger.setLevel(logging.INFO)
-        coloredlogs.install(fmt=MY_FORMAT,level=logging.INFO)
+        coloredlogs.install(fmt=MY_FORMAT, level=logging.INFO)
     else:
         my_logger.setLevel(logging.WARNING)
-        coloredlogs.install(fmt=MY_FORMAT,level=logging.WARNING)
+        coloredlogs.install(fmt=MY_FORMAT, level=logging.WARNING)
     if DEBUG:
         my_logger.setLevel(logging.DEBUG)
-        coloredlogs.install(fmt=MY_FORMAT,level=logging.DEBUG)
+        coloredlogs.install(fmt=MY_FORMAT, level=logging.DEBUG)
     return my_logger
-
--- spectractor.py
+++ spectractor.py
@@ -1,51 +1,57 @@
-import sys,os
-
-from spectroscopy import *
 from images import *
-import parameters 
+from spectroscopy import *
 
 
-def Spectractor(filename,outputdir,guess,target,atmospheric_lines=True,line_detection=False):
+def Spectractor(filename, outputdir, guess, target, atmospheric_lines=True, line_detection=False):
     """ Spectractor
     Main function to extract a spectrum from an image
 
     Args:
-        filename (:obj:`str`): path to the image
-        outputdir (:obj:`str`): path to the output directory
+        filename (str):
+        outputdir: 
+        guess: 
+        target: 
+        atmospheric_lines: 
+        line_detection: 
+
+    Returns:
+        spectrum:
     """
     my_logger = parameters.set_logger(__name__)
     my_logger.info('\n\tStart SPECTRACTOR')
     # Load reduced image
-    image = Image(filename,target=target)
+    image = Image(filename, target=target)
     if parameters.DEBUG:
-        image.plot_image(scale='log10',target_pixcoords=guess)
+        image.plot_image(scale='log10', target_pixcoords=guess)
     # Set output path
     ensure_dir(outputdir)
     output_filename = filename.split('/')[-1]
-    output_filename = output_filename.replace('.fits','_spectrum.fits')
-    output_filename = os.path.join(outputdir,output_filename)
-    
+    output_filename = output_filename.replace('.fits', '_spectrum.fits')
+    output_filename = os.path.join(outputdir, output_filename)
+
     # Test if file already exists
-    #if os.path.exists(output_filename) and os.path.getsize(output_filename)>20000:       
+    # if os.path.exists(output_filename) and os.path.getsize(output_filename)>20000:
     #    filesize= os.path.getsize(output_filename)
     #    infostring=" !!!!!! Spectrum file file %s of size %d already exists, thus SKIP the reconstruction ..." % (output_filename,filesize)
     #    my_logger.info(infostring)
     #    return
-    
+
     # Find the exact target position in the raw cut image: several methods
     my_logger.info('\n\tSearch for the target in the image...')
-    #if parameters.DEBUG:
+    # if parameters.DEBUG:
     #    target_pixcoords = image.find_target_1Dprofile(guess)
     target_pixcoords = image.find_target(guess)
     # Rotate the image: several methods
     image.turn_image()
     # Find the exact target position in the rotated image: several methods
     my_logger.info('\n\tSearch for the target in the rotated image...')
-    #if parameters.DEBUG:
+    # if parameters.DEBUG:
     #    target_pixcoords_rotated = image.find_target_1Dprofile(guess,rotated=True)
-    target_pixcoords_rotated = image.find_target(guess,rotated=True)
+    target_pixcoords_rotated = image.find_target(guess, rotated=True)
+    # Create Spectrum object
+    spectrum = Spectrum(Image=image)
     # Subtract background and bad pixels
-    spectrum = image.extract_spectrum_from_image()
+    image.extract_spectrum_from_image(spectrum)
     spectrum.atmospheric_lines = atmospheric_lines
     # Calibrate the spectrum
     spectrum.calibrate_spectrum()
@@ -58,51 +64,48 @@ def Spectractor(filename,outputdir,guess,target,atmospheric_lines=True,line_dete
     else:
         spectrum.header['WARNINGS'] = 'No calibration procedure with spectral features.'
     # Save the spectrum
-    spectrum.save_spectrum(output_filename,overwrite=True)
+    spectrum.save_spectrum(output_filename, overwrite=True)
     # Plot the spectrum
     if parameters.VERBOSE:
-        if os.getenv("DISPLAY") : spectrum.plot_spectrum(xlim=None,nofit=False)
+        if os.getenv("DISPLAY"): spectrum.plot_spectrum(xlim=None, nofit=False)
     return spectrum
 
-    
 
 if __name__ == "__main__":
-    import commands, string, re, time, os
+    import os
     from optparse import OptionParser
 
     parser = OptionParser()
-    parser.add_option("-d", "--debug", dest="debug",action="store_true",
-                      help="Enter debug mode (more verbose and plots).",default=False)
-    parser.add_option("-v", "--verbose", dest="verbose",action="store_true",
-                      help="Enter verbose (print more stuff).",default=False)
+    parser.add_option("-d", "--debug", dest="debug", action="store_true",
+                      help="Enter debug mode (more verbose and plots).", default=False)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true",
+                      help="Enter verbose (print more stuff).", default=False)
     parser.add_option("-o", "--output_directory", dest="output_directory", default="test/",
                       help="Write results in given output directory (default: ./tests/).")
     (opts, args) = parser.parse_args()
-
 
     parameters.VERBOSE = opts.verbose
     if opts.debug:
         parameters.DEBUG = True
         parameters.VERBOSE = True
-        
-        
-    filename = "../../CTIODataJune2017_reducedRed/data_05jun17/reduc_20170605_005.fits"
-    #filename = "notebooks/fits/trim_20170605_005.fits"
-    filename="../CTIOAnaJun2017/ana_05jun17/OverScanRemove/trim_images/trim_20170605_005.fits"
-    guess = [745,643]
+
+    # filename = "../../CTIODataJune2017_reducedRed/data_05jun17/reduc_20170605_005.fits"
+    # filename = "notebooks/fits/trim_20170605_005.fits"
+    filename = "../CTIOAnaJun2017/ana_05jun17/OverScanRemove/trim_images/trim_20170605_005.fits"
+    guess = [745, 643]
     target = "3C273"
 
-    #filename="../CTIOAnaJun2017/ana_05jun17/OverScanRemove/trim_images/trim_20170605_028.fits"
-    #guess = [814, 585]
-    #target = "PNG321.0+3.9"
-    #filename="../CTIOAnaJun2017/ana_05jun17/OverScanRemove/trim_images/trim_20170605_026.fits"
-    #guess = [735, 645]
-    #target = "PNG321.0+3.9"
-    #filename="../CTIOAnaJun2017/ana_29may17/OverScanRemove/trim_images/trim_20170529_150.fits"
-    #guess = [720, 670]
-    #target = "HD185975"
-    #filename="../CTIOAnaJun2017/ana_31may17/OverScanRemove/trim_images/trim_20170531_150.fits"
-    #guess = [840, 530]
-    #target = "HD205905"
+    # filename="../CTIOAnaJun2017/ana_05jun17/OverScanRemove/trim_images/trim_20170605_028.fits"
+    # guess = [814, 585]
+    # target = "PNG321.0+3.9"
+    # filename="../CTIOAnaJun2017/ana_05jun17/OverScanRemove/trim_images/trim_20170605_026.fits"
+    # guess = [735, 645]
+    # target = "PNG321.0+3.9"
+    # filename="../CTIOAnaJun2017/ana_29may17/OverScanRemove/trim_images/trim_20170529_150.fits"
+    # guess = [720, 670]
+    # target = "HD185975"
+    # filename="../CTIOAnaJun2017/ana_31may17/OverScanRemove/trim_images/trim_20170531_150.fits"
+    # guess = [840, 530]
+    # target = "HD205905"
 
-    Spectractor(filename,opts.output_directory,guess,target)
+    Spectractor(filename, opts.output_directory, guess, target)
