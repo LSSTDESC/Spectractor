6cd6515835b31fa7958136c37bf3e28113be8631
Jérémy Neveu <jneveu@lal.in2p3.fr>
2018-06-12 11:11:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ setup(
     name='Spectractor',
     version='1.0',
     packages=['spectractor'],
-    install_requires=['astropy','astroquery']
+    install_requires=['numpy','astropy','astroquery']
+    test_suite='nose.collector',
+    tests_require=['nose'],
     url='https://github.com/LSSTDESC/Spectractor',
     license='',
     author='J. Neveu, S. Dagoret-Campagne',
