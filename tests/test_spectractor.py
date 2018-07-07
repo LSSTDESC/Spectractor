bac52f9859dac1001b9e70d5f2c08e348d6c7e3c
Jérémy Neveu <jneveu@lal.in2p3.fr>
2018-07-07 16:07:19 +0200

    add doctest to spectractor

--- tests/test_spectractor.py
+++ tests/test_spectractor.py
@@ -10,14 +10,15 @@ def test_spectractor():
     file_names = ['./tests/data/reduc_20170605_028.fits']
 
     logbook = LogBook(logbook='./ctiofulllogbook_jun2017_v5.csv')
-    parameters.VERBOSE = True
+    parameters.DEBUG = True
 
     for file_name in file_names:
         tag = file_name.split('/')[-1]
         target, xpos, ypos = logbook.search_for_image(tag)
         if target is None or xpos is None or ypos is None:
             continue
-        spectrum = Spectractor(file_name, './outputs/', [xpos, ypos], target)
+        spectrum = Spectractor(file_name, './outputs/', [xpos, ypos], target,
+                               line_detection=True, atmospheric_lines=True)
         assert spectrum.data is not None
         assert os.path.isfile('./outputs/' + tag.replace('.fits', '_spectrum.fits')) is True
 
