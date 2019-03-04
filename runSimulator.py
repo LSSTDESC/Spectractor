0c7915b743e80162c931ffdbb9c6017bd781edac
Jérémy Neveu <jneveu@lal.in2p3.fr>
2019-03-04 14:02:25 +0100

    repair tests

--- runSimulator.py
+++ runSimulator.py
@@ -1,6 +1,6 @@
 from spectractor import parameters
-from spectractor.simulation.simulator import Simulator, SimulatorSimGrid, Atmosphere, AtmosphereGrid
-
+from spectractor.simulation.simulator import SpectrumSimulator, Atmosphere, AtmosphereGrid
+from spectractor.config import load_config
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -15,8 +15,10 @@ if __name__ == "__main__":
                         help="Enter verbose (print more stuff).", default=False)
     parser.add_argument("-o", "--output_directory", dest="output_directory", default="outputs/",
                         help="Write results in given output directory (default: ./outputs/).")
-    parser.add_argument("-c", "--csv", dest="csv", default="ctiofulllogbook_jun2017_v5.csv",
+    parser.add_argument("-l", "--logbook", dest="logbook", default="ctiofulllogbook_jun2017_v5.csv",
                         help="CSV logbook file. (default: ctiofulllogbook_jun2017_v5.csv).")
+    parser.add_argument("-c", "--config", dest="config", default="config/ctio.ini",
+                        help="INI config file. (default: config.ctio.ini).")
     args = parser.parse_args()
 
     parameters.VERBOSE = args.verbose
@@ -26,9 +28,11 @@ if __name__ == "__main__":
 
     file_names = args.input
 
+    load_config(args.config)
+
     for file_name in file_names:
-        spectrum_simulation = Simulator(file_name, pwv=3.1, ozone=387, aerosols=0.091,
+        spectrum_simulation = SpectrumSimulator(file_name, pwv=3.1, ozone=387, aerosols=0.091,
                                         A1=1.1, A2=0.15, reso=2.5, D=55.26, shift=-0.2)
         atmgrid = AtmosphereGrid(file_name)
         atm = Atmosphere(atmgrid.airmass, atmgrid.pressure, atmgrid.temperature)
-        SimulatorSimGrid(file_name, args.output_directory)
+        # SpectrogramSimulatorSimGrid(file_name, args.output_directory)
