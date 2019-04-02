ed73964f72889de7423901eef294c712445474dd
sylvielsstfr <sylvielsstfr@users.noreply.github.com>
2019-04-02 16:20:03 +0200

    produce images of spectra

---
+++ README_PicDuMidi.md
@@ -0,0 +1,22 @@
+# README.md for Pic Du Midi
+#-----------------------------------------------
+Adaptation of Spectractor to read images from Pic Du Midi
+This work is performed in branch pic-du-midi_19-sylv of Spectractor package
+
+
+- author : Sylvie Dagoret-Campagne
+- date : April 2nd 2019
+
+
+## Initial phase
+- the images must be first reduced and rotated by 45° before being given to Spectractor. This task is performed by the package
+
+- ** https://gitlab.in2p3.fr/dagoret/picdumidi **
+
+
+## To produce spectra
+	runPDMExtractor.py
+	
+	
+## To view Spectra
+	runViewSpectra.py
---
+++ runViewSpectra.py
@@ -0,0 +1,122 @@
+#########################################################################################################
+# View spectra produced in Spectractor
+#########################################################################################################
+
+import os
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
+import re
+
+import matplotlib.pyplot as plt
+
+if not 'workbookDir' in globals():
+    workbookDir = os.getcwd()
+print('workbookDir: ' + workbookDir)
+
+
+import sys
+sys.path.append(workbookDir)
+sys.path.append(os.path.dirname(workbookDir))
+
+from spectractor import parameters
+from spectractor.extractor.extractor import Spectractor
+from spectractor.logbook import LogBook
+from spectractor.extractor.dispersers import *
+from spectractor.extractor.spectrum import *
+
+
+
+if __name__ == "__main__":
+
+    #####################
+    # 1) Configuration
+    ######################
+
+    parameters.VERBOSE = True
+    parameters.DEBUG = True
+
+    # thedate="20190214"
+    thedate = "20190215"
+
+    output_directory = "output/" + thedate
+
+    parameters.VERBOSE = True
+    parameters.DISPLAY = True
+
+
+
+    ############################
+    # 2) Get Spectra filelist
+    #########################
+
+
+    # get all files
+    onlyfiles = [f for f in listdir(output_directory) if isfile(join(output_directory, f))]
+    onlyfiles = np.array(onlyfiles)
+
+    # sort files
+    sortedindexes = np.argsort(onlyfiles)
+    onlyfiles = onlyfiles[sortedindexes]
+
+    # get only _spectrum.fits file
+    onlyfilesspectrum = []
+    for f in onlyfiles:
+        if re.search("^T.*_spectrum.fits$", f):
+            onlyfilesspectrum.append(re.findall("(^T.*_spectrum.fits$)", f)[0])
+
+
+
+    onlyfilesspectrum = np.array(onlyfilesspectrum)
+    sortedindexes = np.argsort(onlyfilesspectrum)
+    onlyfilesspectrum = onlyfilesspectrum[sortedindexes]
+
+
+    #get basnemae of files for later use (to check if _table.csv and _spectrogram.fits exists
+    onlyfilesbasename=[]
+    for f in onlyfilesspectrum:
+        onlyfilesbasename.append(re.findall("(^T.*)_spectrum.fits$",f)[0])
+
+
+    basenamecut=[]
+    for f in onlyfilesspectrum:
+        basenamecut.append(f.split("_HD")[0])
+
+
+    #############################################
+    # 3) Plot Spectra
+    ##########################################
+
+    NBSPEC = len(sortedindexes)
+
+    for idx in np.arange(0, NBSPEC):
+        # if idx in [0,1,4]:
+        #    continue
+
+        print("{}) : {}".format(idx,onlyfilesspectrum[idx]))
+
+        fullfilename = os.path.join(output_directory, onlyfilesspectrum[idx])
+        s = Spectrum()
+        s.load_spectrum(fullfilename)
+
+        fig=plt.figure(figsize=[12, 6])
+        ax = plt.gca()
+
+
+        s.plot_spectrum(ax=ax,xlim=None, label=basenamecut[idx],force_lines=True)
+        figfilename="fig_spec_"+basenamecut[idx]+".png"
+
+        fig.savefig(figfilename)
+
+
+
+
+
+
+
+
+
+
+
+
+
