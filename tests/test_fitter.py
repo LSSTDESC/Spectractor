63893e382645b7124e060776e06f99a0568c0e25
Jérémy Neveu <jneveu@lal.in2p3.fr>
2019-09-14 10:09:17 +0200

    add outliers removal

--- tests/test_fitter.py
+++ tests/test_fitter.py
@@ -1,6 +1,6 @@
 from numpy.testing import run_module_suite
 import numpy as np
-from spectractor.fit.fitter import FitWorkspace, run_minimisation, run_emcee
+from spectractor.fit.fitter import FitWorkspace, run_minimisation, run_emcee, run_minimisation_sigma_clipping
 from spectractor.config import set_logger
 from spectractor import parameters
 
@@ -78,6 +78,53 @@ def test_fitworkspace():
     assert os.path.exists(file_name.replace(".txt", "_emcee_convergence.pdf"))
     assert os.path.exists(file_name.replace(".txt", "_emcee_triangle.pdf"))
     assert np.all([np.abs(w.p[i] - truth[i]) / np.sqrt(w.cov[i,i]) < 2 for i in range(w.ndim)])
+
+
+def test_minimisation_sigma_clipping():
+    # Create mock data
+    N = 100
+    a = 5
+    b = -1
+    truth = (a, b)
+    sigma = 0.1
+    x = np.linspace(0, 1, N)
+    y = a * x + b
+    yerr = sigma * np.ones_like(y)
+    y += np.random.normal(scale=sigma, size=N)
+    parameters.VERBOSE = True
+
+    # Add outliers
+    outliers = [3, 4, 5, 6, 7, 22, 23, 24, 25, 26, 27, 28, 29, 80, 81]
+    y[outliers[:10]] = 4
+    y[outliers[10:]] = -3
+
+    # Do the fits
+    sigma = 5
+    clip_niter = 4
+    w = LineFitWorkspace(x, y, yerr, file_name="", truth=truth, nwalkers=20, nsteps=5000, burnin=1000, nbins=20)
+    run_minimisation_sigma_clipping(w, method="minimize", sigma=sigma, clip_niter=clip_niter)
+    assert np.all([np.abs(w.p[i] - truth[i]) / sigma < 1 for i in range(w.ndim)])
+    assert np.all(outliers == w.outliers)
+    # w.p = np.array([1, 1])
+    # w.outliers = []
+    # run_minimisation_sigma_clipping(w, method="basinhopping", sigma=sigma, clip_niter=clip_niter)
+    # assert np.all([np.abs(w.p[i] - truth[i]) / sigma < 1 for i in range(w.ndim)])
+    # assert np.all(outliers == w.outliers)
+    # w.p = np.array([1, 1])
+    # w.outliers = []
+    # run_minimisation_sigma_clipping(w, method="least_squares", sigma=sigma, clip_niter=clip_niter)
+    # assert np.all([np.abs(w.p[i] - truth[i]) / sigma < 1 for i in range(w.ndim)])
+    # assert np.all(outliers == w.outliers)
+    # w.p = np.array([1, 1])
+    # w.outliers = []
+    # run_minimisation_sigma_clipping(w, method="minuit", sigma=sigma, clip_niter=clip_niter)
+    # assert np.all([np.abs(w.p[i] - truth[i]) / sigma < 1 for i in range(w.ndim)])
+    # assert np.all(outliers == w.outliers)
+    w.p = np.array([1, 1])
+    w.outliers = []
+    run_minimisation_sigma_clipping(w, method="newton", sigma=sigma, clip_niter=clip_niter)
+    assert np.all([np.abs(w.p[i] - truth[i]) / sigma < 1 for i in range(w.ndim)])
+    assert np.all(outliers == w.outliers)
 
 
 if __name__ == "__main__":
