6ce3a09db37528f7b0a1a078413d9f306cac1d72
Jérémy Neveu <jneveu@lal.in2p3.fr>
2019-09-21 17:30:54 +0200

    remove heavy plots

--- spectractor/extractor/images.py
+++ spectractor/extractor/images.py
@@ -503,15 +503,6 @@ def find_target_1Dprofile(image, sub_image, guess):
     --------
     >>> im = Image('tests/data/reduc_20170605_028.fits')
     >>> guess = [820, 580]
-
-    .. plot::
-
-        import matplotlib.pyplot as plt
-        import numpy as np
-        from spectractor.extractor.images import Image
-        im = Image('tests/data/reduc_20170605_028.fits')
-        im.plot_image(target_pixcoords=[820, 580])
-
     >>> parameters.DEBUG = True
     >>> sub_image, x0, y0, Dx, Dy, sub_errors = find_target_init(im, guess, rotated=False)
     >>> x1, y1 = find_target_1Dprofile(im, sub_image, guess)
@@ -593,15 +584,6 @@ def find_target_2Dprofile(image, sub_image, guess, sub_errors=None):
     --------
     >>> im = Image('tests/data/reduc_20170605_028.fits')
     >>> guess = [820, 580]
-
-    .. plot::
-
-        import matplotlib.pyplot as plt
-        import numpy as np
-        from spectractor.extractor.images import Image
-        im = Image('tests/data/reduc_20170605_028.fits')
-        im.plot_image(target_pixcoords=[820, 580])
-
     >>> parameters.DEBUG = True
     >>> sub_image, x0, y0, Dx, Dy, sub_errors = find_target_init(im, guess, rotated=False)
     >>> xmax = np.argmax(np.sum(sub_image, axis=0))
@@ -730,23 +712,6 @@ def compute_rotation_angle_hessian(image, deg_threshold=10, width_cut=parameters
     >>> for x in np.arange(N):
     ...     im.data[int(y(x)), x] = 10
     ...     im.data[int(y(x))+1, x] = 10
-
-    .. plot::
-
-        from spectractor.extractor.images import Image
-        import spectractor.parameters as parameters
-        im=Image('tests/data/reduc_20170605_028.fits', disperser_label='HoloPhAg')
-        N = parameters.CCD_IMSIZE
-        N = parameters.CCD_IMSIZE
-        im.data = np.ones((N, N))
-        slope = -0.1
-        y = lambda x: slope * (x - 0.5*N) + 0.5*N
-        for x in np.arange(N):
-            im.data[int(y(x)), x] = 10
-            im.data[int(y(x))+1, x] = 10
-        plt.imshow(im.data, origin='lower')
-        plt.show()
-
     >>> im.target_pixcoords=(N//2, N//2)
     >>> parameters.DEBUG = True
     >>> theta = compute_rotation_angle_hessian(im)
