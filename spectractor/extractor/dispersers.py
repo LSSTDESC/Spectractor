2b3459a92169c9d6b6dc6ba9ae2b1e9533dd456c
Jérémy Neveu <jneveu@lal.in2p3.fr>
2019-12-27 19:07:23 +0100

    repair doctests

--- spectractor/extractor/dispersers.py
+++ spectractor/extractor/dispersers.py
@@ -712,4 +712,7 @@ class Hologram(Grating):
 if __name__ == "__main__":
     import doctest
 
+    if np.__version__ >= "1.14.0":
+        np.set_printoptions(legacy="1.13")
+
     doctest.testmod()
