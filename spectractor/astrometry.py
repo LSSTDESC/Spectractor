60d762284a5d107f6caeccd12c7f839c50855d52
Jérémy Neveu <jneveu@lal.in2p3.fr>
2019-12-27 17:29:06 +0100

    working first wcs building

--- spectractor/astrometry.py
+++ spectractor/astrometry.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.stats import SigmaClip, sigma_clipped_stats
@@ -6,11 +7,12 @@ from astropy.io import fits
 
 from photutils import Background2D, SExtractorBackground, DAOStarFinder
 from spectractor import parameters
-from spectractor.tools import plot_image_simple
+from spectractor.tools import plot_image_simple, ensure_dir
 from spectractor.config import set_logger
 from spectractor.extractor.images import Image
 
-def remove_background(data, sigma=3.0, box_size=(50,50), filter_size=(3,3)):
+
+def remove_background(data, sigma=3.0, box_size=(50, 50), filter_size=(3, 3)):
     sigma_clip = SigmaClip(sigma=sigma)
     bkg_estimator = SExtractorBackground()
     bkg = Background2D(data, box_size, filter_size=filter_size,
@@ -41,22 +43,72 @@ def source_detection(data_wo_bkg, sigma=3.0, fwhm=3.0, threshold_std_factor=3.5)
     return sources
 
 
-def run_astrometry(file_name, extent=None):
+def run_astrometry(file_name, guess, target, disperser_label="", extent=None):
+    """Build a World Coordinate System (WCS) using astrometry.net library given an exposure as a FITS file.
+
+    The name of the target must be given to get its RA,DEC coordinates via a Simbad query.
+    First the background of the exposure is removed using the astropy SExtractorBackground() method.
+    Then photutils source_detection() is used to get the positions in pixels en flux of the objects in the field.
+    The results are saved in the {file_name}_sources.fits file and used by the solve_field command from the
+    astrometry.net library. The solve_field path must be set using the spectractor.parameters.ASTROMETRYNET_BINDIR
+    variable. A new WCS is created and merged with the given exposure.
+    The intermediate results are saved in a new directory named as the FITS file name with a _wcs suffix.
+
+    Parameters
+    ----------
+    file_name: str
+        Input file nam of the image to analyse.
+    guess: [int,int]
+        [x0,y0] list of the guessed pixel positions of the target in the image (must be integers).
+    target: str
+        The name of the targeted object.
+    disperser_label: str
+        The name of the disperser (default: "").
+    extent: 2-tuple
+        ((ymin,ymax),(xmin,xmax)) 2 dimensional typle to crop the exposure before any operation (default: None).
+
+    Examples
+    --------
+
+    >>> import os
+    >>> from spectractor.logbook import LogBook
+    >>> from spectractor import parameters
+    >>> parameters.VERBOSE = True
+    >>> logbook = LogBook(logbook='./ctiofulllogbook_jun2017_v5.csv')
+    >>> file_names = ['./tests/data/reduc_20170530_134.fits']
+    >>> for file_name in file_names:
+    ...     tag = file_name.split('/')[-1]
+    ...     disperser_label, target, xpos, ypos = logbook.search_for_image(tag)
+    ...     if target is None or xpos is None or ypos is None:
+    ...         continue
+    ...     run_astrometry(file_name, [xpos, ypos], target, disperser_label)
+    ...     assert os.path.isdir('./tests/data/reduc_20170530_134_wcs')
+
+
+    """
     my_logger = set_logger(__name__)
     # load image
     im = Image(file_name, target=target, disperser_label=disperser_label)
+    # prepare outputs
+    output_directory = os.path.join(os.path.dirname(file_name), os.path.splitext(os.path.basename(file_name))[0]) \
+                       + "_wcs"
+    my_logger.info(f"\n\tIntermediate outputs will be stored in {output_directory}")
+    ensure_dir(output_directory)
+    tag = os.path.splitext(os.path.basename(file_name))[0]
+    # crop data
     if extent is not None:
         data = im.data[extent[0][0]:extent[0][1], extent[1][0]:extent[1][1]]
     else:
         data = np.copy(im.data)
     if parameters.DEBUG:
         im.plot_image(scale="log10")
-    if parameters.VERBOSE:
-        my_logger.info('\n\tRemove background using astropy SExtractorBackground()...')
+    # remove background
+    my_logger.info('\n\tRemove background using astropy SExtractorBackground()...')
     data_wo_bkg = remove_background(data)
-    if parameters.VERBOSE:
-        my_logger.info('\n\tDetect sources using photutils...')
+    # extract source positions and fluxes
+    my_logger.info('\n\tDetect sources using photutils source_detection()...')
     sources = source_detection(data_wo_bkg)
+    # write results in fits file
     colx = fits.Column(name='X', format='D', array=sources['xcentroid'])
     coly = fits.Column(name='Y', format='D', array=sources['ycentroid'])
     colflux = fits.Column(name='FLUX', format='D', array=sources['flux'])
@@ -64,16 +116,30 @@ def run_astrometry(file_name, extent=None):
     hdu = fits.BinTableHDU.from_columns(coldefs)
     hdu.header['IMAGEW'] = data.shape[1]
     hdu.header['IMAGEH'] = data.shape[0]
-    hdu.writeto(f"astrometry/{tag}_sources.fits", overwrite=True)
-    if parameters.VERBOSE:
-        my_logger.info(f'\n\tSources positions saved in {oihohi}')
-    command = f"{parameters.ASTROMETRYNET_BINDIR}/solve-field --scale-unit arcsecperpix " \
-              f"--scale-low {0.9*parameters.CCD_PIXEL2ARCSEC} " \
-              f"--scale-high {1.1*parameters.CCD_PIXEL2ARCSEC} " \
-              f"--ra {im.target.coord.ra.value} --dec {im.target.coord.dec.value} --radius 0.5 " \
-              f"--dir astrometry --out {tag} " \
-              f"--overwrite --x-column X --y-column Y astrometry/{tag}_sources.fits"
-    if parameters.VERBOSE:
-        my_logger.info(f'\n\tRun astrometry.net solve_field:\n\t{command}')
-
+    output_sources_fitsfile = f"{output_directory}/{tag}_sources.fits"
+    hdu.writeto(output_sources_fitsfile, overwrite=True)
+    my_logger.info(f'\n\tSources positions saved in {output_sources_fitsfile}')
+    # run astrometry.net
+    command = f"{os.path.join(parameters.ASTROMETRYNET_BINDIR,'solve-field')} --scale-unit arcsecperpix " \
+              f"--scale-low {0.9 * parameters.CCD_PIXEL2ARCSEC} " \
+              f"--scale-high {1.1 * parameters.CCD_PIXEL2ARCSEC} " \
+              f"--ra {im.target.coord.ra.value} --dec {im.target.coord.dec.value} " \
+              f"--radius {2 * parameters.CCD_IMSIZE * parameters.CCD_PIXEL2ARCSEC / 3600.} " \
+              f"--dir {output_directory} --out {tag} " \
+              f"--overwrite --x-column X --y-column Y {output_sources_fitsfile}"
+    my_logger.info(f'\n\tRun astrometry.net solve_field command:\n\t{command}')
+    log = subprocess.check_output(command, shell=True)
+    log_file = open(f"{output_directory}/{tag}.log", "w+")
+    log_file.write(command+"\n")
+    log_file.write(log.decode("utf-8")+"\n")
+    # save new WCS in original fits file
+    new_file_name = file_name.replace('.fits','_new.fits')
+    command = f"{os.path.join(parameters.ASTROMETRYNET_BINDIR, 'new-wcs')} -v -d -i {file_name} " \
+              f"-w {os.path.join(output_directory, tag+'.wcs')} -o {new_file_name}\n"
+    # f"mv {new_file_name} {file_name}"
+    my_logger.info(f'\n\tSave WCS in original file:\n\t{command}')
+    log = subprocess.check_output(command, shell=True)
+    log_file.write(command+"\n")
+    log_file.write(log.decode("utf-8")+"\n")
+    log_file.close()
 
