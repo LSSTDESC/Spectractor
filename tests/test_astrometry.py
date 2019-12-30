f468c7dec297c68e342f8585e383203024df0893
Jérémy Neveu <jneveu@lal.in2p3.fr>
2019-12-30 16:48:11 +0100

    make test_astrometry produce wcs files for other unit tests

--- tests/test_astrometry.py
+++ tests/test_astrometry.py
@@ -4,6 +4,7 @@ from spectractor import parameters
 from spectractor.astrometry import Astrometry
 from spectractor.logbook import LogBook
 from spectractor.config import load_config
+from spectractor.tools import set_wcs_output_directory, set_wcs_file_name
 import os
 import subprocess
 import numpy as np
@@ -11,20 +12,19 @@ import astropy.units as u
 
 
 def test_astrometry():
-    file_names = ['tests/data/reduc_20170605_028.fits']
-    if os.path.isfile('./tests/data/reduc_20170605_028_new.fits'):
-        os.remove('./tests/data/reduc_20170605_028_new.fits')
-    if os.path.isdir('./tests/data/reduc_20170605_028_wcs'):
-        subprocess.Popen(["rm", "-rf",  "./tests/data/reduc_20170605_028_wcs"])
+    file_names = ['tests/data/reduc_20170605_028.fits', 'tests/data/reduc_20170530_134.fits']
 
     load_config('./config/ctio.ini')
     logbook = LogBook(logbook='./ctiofulllogbook_jun2017_v5.csv')
-    parameters.DEBUG = True
+    # parameters.DEBUG = True
 
     radius = 500
     maxiter = 10
 
     for file_name in file_names:
+        wcs_output_directory = set_wcs_output_directory(file_name)
+        if os.path.isdir(wcs_output_directory):
+            subprocess.check_output(f"rm -rf {wcs_output_directory}", shell=True)
         tag = file_name.split('/')[-1]
         disperser_label, target, xpos, ypos = logbook.search_for_image(tag)
         if target is None or xpos is None or ypos is None:
@@ -44,16 +44,17 @@ def test_astrometry():
             a.plot_sources_and_gaia_catalog(sources=a.sources, gaia_coord=a.gaia_matches, margin=200)
             a.plot_astrometry_shifts(vmax=3)
         # checks
-        assert os.path.isdir('./tests/data/reduc_20170605_028_wcs')
-        assert os.path.isfile('./tests/data/reduc_20170605_028_wcs/reduc_20170605_028.wcs')
+        assert os.path.isdir(wcs_output_directory)
+        assert os.path.isfile(set_wcs_file_name(file_name))
         assert a.data is not None
         assert np.sum(a.data) > 1e-10
-        assert len(a.sources) > 400
-        assert np.isclose(a.target_coord_after_motion.ra.value, 224.97283917)
-        assert np.isclose(a.target_coord_after_motion.dec.value, -54.30209)
-        assert np.isclose(a.wcs.wcs.crval[0], 224.9718998)
-        assert np.isclose(a.wcs.wcs.crval[1], -54.28912925)
         assert np.all(np.abs([dra_median, ddec_median]) < 1e-3)
+        if file_name == 'tests/data/reduc_20170605_028.fits':
+            assert len(a.sources) > 400
+            assert np.isclose(a.target_coord_after_motion.ra.value, 224.97283917)
+            assert np.isclose(a.target_coord_after_motion.dec.value, -54.30209)
+            assert np.isclose(a.wcs.wcs.crval[0], 224.9718998)
+            assert np.isclose(a.wcs.wcs.crval[1], -54.28912925)
 
 
 if __name__ == "__main__":
