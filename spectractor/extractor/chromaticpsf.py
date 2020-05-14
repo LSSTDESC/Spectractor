1693be61ad99c2feca849606ae39194c4af92977
Jérémy Neveu <jneveu@lal.in2p3.fr>
2020-05-14 17:14:04 +0200

    remove unuseful save test.txt

--- spectractor/extractor/chromaticpsf.py
+++ spectractor/extractor/chromaticpsf.py
@@ -1138,7 +1138,6 @@ class ChromaticPSFFitWorkspace(FitWorkspace):
             # xx, yy = np.meshgrid(np.arange(Nx), pixels)
             self.bgd = self.bgd_model_func(np.arange(self.Nx), self.pixels)
         self.data = self.data - self.bgd
-        np.savetxt("test.txt", self.data)
         self.bgd_std = float(np.std(np.random.poisson(self.bgd)))
 
         # crop spectrogram to fit faster
