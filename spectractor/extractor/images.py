829a7c671483d76f631e066dd33e0977df887d56
Jérémy Neveu <jneveu@lal.in2p3.fr>
2020-06-16 11:17:54 +0200

    adapt load_auxtel for ADR

--- spectractor/extractor/images.py
+++ spectractor/extractor/images.py
@@ -586,6 +586,15 @@ def load_AUXTEL_image(image):  # pragma: no cover
     parameters.CCD_IMSIZE = image.data.shape[1]
     image.disperser_label = image.header['GRATING']
     image.read_out_noise = np.zeros_like(image.data)
+    # image.dec = image.header['DEC']
+    # image.hour_angle = image.header['HA']
+    # image.temperature = image.header['OUTTEMP']
+    # image.pressure = image.header['OUTPRESS']
+    # image.humidity = image.header['OUTHUM']
+    # image.xpixsize = image.header['XPIXSIZE']
+    # image.ypixsize = image.header['YPIXSIZE']
+    parameters.OBS_ALTITUDE = image.header['OBS-ELEV']
+    parameters.OBS_LATITUDE = image.header['OBS-LAT']
 
 
 def find_target(image, guess=None, rotated=False, use_wcs=True):
