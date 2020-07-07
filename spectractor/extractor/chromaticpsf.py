debug: multiply final cov matrix with the ratio of traces (instead of taking the prior cov matrix which can have the wrong shape)

spectractor/extractor/chromaticpsf.py
@@ -1099,9 +1099,10 @@ class ChromaticPSF:
                                        f"below the trace of the prior covariance matrix "
                                        f"({np.trace(w.amplitude_priors_cov_matrix)}). This is probably due to a very "
                                        f"high regularisation parameter in case of a bad fit. Therefore the final "
-                                       f"covariance matrix is set at the value of the prior covariance matrix and "
+                                       f"covariance matrix is mulitiplied by the ratio of the traces and "
                                        f"the amplitude parameters are very close the amplitude priors.")
-                w.amplitude_cov_matrix = np.copy(w.amplitude_priors_cov_matrix)
+                r = np.trace(w.amplitude_priors_cov_matrix) / np.trace(w.amplitude_cov_matrix)
+                w.amplitude_cov_matrix *= r
                 w.amplitude_params_err = np.array([np.sqrt(w.amplitude_cov_matrix[x, x]) for x in range(self.Nx)])
 
         self.poly_params = w.poly_params