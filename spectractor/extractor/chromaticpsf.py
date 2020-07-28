1570c8062c5e0e81b8259ca5af81f2022088c66a
Jérémy Neveu <jneveu@lal.in2p3.fr>
2020-07-28 16:21:38 +0200

    20 sigma clipping for deconvolution

--- spectractor/extractor/chromaticpsf.py
+++ spectractor/extractor/chromaticpsf.py
@@ -1081,7 +1081,7 @@ class ChromaticPSF:
                                            amplitude_priors_method=amplitude_priors_method, verbose=verbose, live_fit=True)
             # run_minimisation(w, method="newton", ftol=1 / (w.Nx * w.Ny), xtol=1e-6, niter=50, fix=w.fixed)
             run_minimisation_sigma_clipping(w, method="newton", ftol=10 / (w.Nx * w.Ny), xtol=1e-6, niter=50,
-                                            fix=w.fixed, sigma_clip=10, niter_clip=3, verbose=verbose)
+                                            fix=w.fixed, sigma_clip=20, niter_clip=3, verbose=verbose)
         else:
             self.my_logger.error(f"\n\tUnknown fitting mode={mode}. Must be '1D' or '2D'.")
 
