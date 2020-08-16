Implement methods to fit with a covariance matrix coming from data (and with outliers); change fit_spectrum starting points and fix A2 and B

spectractor/fit/fit_spectrum.py
@@ -76,11 +76,12 @@ class SpectrumFitWorkspace(FitWorkspace):
         self.lambdas = self.spectrum.lambdas
         self.data = self.spectrum.data
         self.err = self.spectrum.err
+        self.data_cov = self.spectrum.cov_matrix
         self.A1 = 1.0
         self.A2 = 0
-        self.ozone = 300.
-        self.pwv = 5
-        self.aerosols = 0.03
+        self.ozone = 400.
+        self.pwv = 3
+        self.aerosols = 0.05
         self.reso = -1
         self.D = self.spectrum.header['D2CCD']
         self.shift_x = self.spectrum.header['PIXSHIFT']
@@ -88,12 +89,12 @@ class SpectrumFitWorkspace(FitWorkspace):
         self.p = np.array([self.A1, self.A2, self.ozone, self.pwv, self.aerosols, self.reso, self.D,
                            self.shift_x, self.B])
         self.fixed = [False] * self.p.size
-        # self.fixed[0] = True
+        self.fixed[1] = True
         self.fixed[5] = True
         # self.fixed[6:8] = [True, True]
         # self.fixed[7] = False
         # self.fixed[1] = True
-        # self.fixed[-1] = True
+        self.fixed[-1] = True
         self.input_labels = ["A1", "A2", "ozone", "PWV", "VAOD", "reso [pix]", r"D_CCD [mm]",
                              r"alpha_pix [pix]", "B"]
         self.axis_names = ["$A_1$", "$A_2$", "ozone", "PWV", "VAOD", "reso [pix]", r"$D_{CCD}$ [mm]",
@@ -234,34 +235,6 @@ class SpectrumFitWorkspace(FitWorkspace):
         self.model_err = model_err
         return lambdas, model, model_err
 
-    def weighted_residuals(self, p):
-        """Computation of the weighted residuals between model and data using a covariance matrix.
-
-        Parameters
-        ----------
-        p: array_like
-            Array of SpectrogramFitWorkspace parameters.
-
-        Returns
-        -------
-        residuals: array_like
-            The 1D residual array.
-
-        """
-        x, model, model_err = self.simulate(*p)
-        if len(self.outliers) > 0:
-            good_indices = np.asarray(self.not_outliers, dtype=int)
-            cov = self.spectrum.cov_matrix + np.diag(model_err * model_err)
-            cov = cov[good_indices[:, None], good_indices]
-            L = np.linalg.inv(np.linalg.cholesky(cov))
-            res = L @ (model[good_indices] - self.data[good_indices])
-            # raise NotImplementedError("Weighted residuals function not implemented for outlier rejection.")
-        else:
-            cov = self.spectrum.cov_matrix + np.diag(model_err * model_err)
-            L = np.linalg.inv(np.linalg.cholesky(cov))
-            res = L @ (model - self.data)
-        return res
-
     def plot_fit(self):
         """Plot the fit result.
 
@@ -396,12 +369,12 @@ def run_spectrum_minimisation(fit_workspace, method="newton"):
         fit_workspace.simulation.fast_sim = True
         fit_workspace.simulation.fix_psf_cube = False
         run_minimisation_sigma_clipping(fit_workspace, method="newton", epsilon=epsilon, fix=fit_workspace.fixed,
-                                        xtol=1e-4, ftol=1 / fit_workspace.data.size, sigma_clip=20, niter_clip=3,
+                                        xtol=1e-4, ftol=1 / fit_workspace.data.size, sigma_clip=10, niter_clip=3,
                                         verbose=False)
 
         fit_workspace.simulation.fast_sim = False
         run_minimisation_sigma_clipping(fit_workspace, method="newton", epsilon=epsilon, fix=fit_workspace.fixed,
-                                        xtol=1e-4, ftol=1 / fit_workspace.data.size, sigma_clip=20, niter_clip=3,
+                                        xtol=1e-4, ftol=1 / fit_workspace.data.size, sigma_clip=10, niter_clip=3,
                                         verbose=False)
         if fit_workspace.filename != "":
             parameters.SAVE = True
@@ -468,7 +441,7 @@ if __name__ == "__main__":
                  'outputs/data_30may17_HoloAmAg_prod6.9/sim_20170530_199_spectrum.fits']
     params = []
     chisqs = []
-    filenames = ['outputs/reduc_20170530_134_spectrum.fits']
+    filenames = ['outputs/sim_20170530_134_spectrum.fits']
     for filename in filenames:
         atmgrid_filename = filename.replace('sim', 'reduc').replace('spectrum', 'atmsim')
 

spectractor/fit/fitter.py
@@ -60,6 +60,7 @@ class FitWorkspace:
         self.rho = np.array([[]])
         self.data = None
         self.err = None
+        self.data_cov = None
         self.x = None
         self.outliers = []
         self.sigma_clip = 5
@@ -575,14 +576,26 @@ class FitWorkspace:
 
         """
         x, model, model_err = self.simulate(*p)
-        if len(self.outliers) > 0:
-            good_indices = self.not_outliers
-            model_err = model_err.flatten()[good_indices]
-            err = self.err.flatten()[good_indices]
-            res = (model.flatten()[good_indices] - self.data.flatten()[good_indices]) / np.sqrt(
-                model_err * model_err + err * err)
+        if self.data_cov is None:
+            if len(self.outliers) > 0:
+                good_indices = self.not_outliers
+                model_err = model_err.flatten()[good_indices]
+                err = self.err.flatten()[good_indices]
+                res = (model.flatten()[good_indices] - self.data.flatten()[good_indices]) / np.sqrt(
+                    model_err * model_err + err * err)
+            else:
+                res = ((model - self.data) / np.sqrt(model_err * model_err + self.err * self.err)).flatten()
         else:
-            res = ((model - self.data) / np.sqrt(model_err * model_err + self.err * self.err)).flatten()
+            if len(self.outliers) > 0:
+                good_indices = np.asarray(self.not_outliers, dtype=int)
+                cov = self.data_cov + np.diag(model_err * model_err)
+                cov = cov[good_indices[:, None], good_indices]
+                L = np.linalg.inv(np.linalg.cholesky(cov))
+                res = L @ (model[good_indices] - self.data[good_indices])
+            else:
+                cov = self.data_cov + np.diag(model_err * model_err)
+                L = np.linalg.inv(np.linalg.cholesky(cov))
+                res = L @ (model - self.data)
         return res
 
     def chisq(self, p):
@@ -754,7 +767,11 @@ def gradient_descent(fit_workspace, params, epsilon, niter=10, fixed_params=None
     """
     my_logger = set_logger(__name__)
     tmp_params = np.copy(params)
-    W = 1 / (fit_workspace.err.flatten()[fit_workspace.not_outliers]) ** 2
+    if fit_workspace.data_cov is None:
+        cov_data = np.asarray(fit_workspace.err.flatten()[fit_workspace.not_outliers] ** 2)
+    else:
+        good_indices = np.asarray(fit_workspace.not_outliers, dtype=int)
+        cov_data = fit_workspace.data_cov[good_indices[:, None], good_indices]
     ipar = np.arange(params.size)
     if fixed_params is not None:
         ipar = np.array(np.where(np.array(fixed_params).astype(int) == 0)[0])
@@ -767,7 +784,21 @@ def gradient_descent(fit_workspace, params, epsilon, niter=10, fixed_params=None
         # if fit_workspace.live_fit:
         #    fit_workspace.plot_fit()
         residuals = (tmp_model - fit_workspace.data).flatten()[fit_workspace.not_outliers]
-        cost = residuals @ (W * residuals)
+        if cov_data.ndim == 1:
+            if np.any(tmp_model_err > 0):
+                cov = cov_data + np.asarray(tmp_model_err.flatten()[fit_workspace.not_outliers] ** 2)
+            else:
+                cov = cov_data
+            W = 1 / cov
+            cost = residuals @ (W * residuals)
+        else:
+            if np.any(tmp_model_err > 0):
+                cov = cov_data + np.diag(tmp_model_err.flatten()[fit_workspace.not_outliers] ** 2)
+            else:
+                cov = cov_data
+            L = np.linalg.inv(np.linalg.cholesky(cov))
+            W = L.T @ L
+            cost = residuals @ W @ residuals
         J = fit_workspace.jacobian(tmp_params, epsilon, fixed_params=fixed_params)
         # remove parameters with unexpected null Jacobian vectors
         for ip in range(J.shape[0]):
@@ -783,7 +814,10 @@ def gradient_descent(fit_workspace, params, epsilon, niter=10, fixed_params=None
         # remove fixed parameters
         J = J[ipar].T
         # algebra
-        JT_W = J.T * W
+        if W.ndim == 1:
+            JT_W = J.T * W
+        else:
+            JT_W = J.T @ W
         JT_W_J = JT_W @ J
         try:
             L = np.linalg.inv(np.linalg.cholesky(JT_W_J))  # cholesky is too sensible to the numerical precision