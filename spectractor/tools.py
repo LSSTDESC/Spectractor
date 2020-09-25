75ef338fe5ffa555ad0d92e81f47e7b21b6313f2
Jérémy Neveu <jneveu@lal.in2p3.fr>
2020-09-25 11:27:42 +0200

    inverse signs for hessian matrix and angles

--- spectractor/tools.py
+++ spectractor/tools.py
@@ -1396,10 +1396,10 @@ def weighted_avg_and_std(values, weights):
 
 def hessian_and_theta(data, margin_cut=1):
     # compute hessian matrices on the image
-    Hxx, Hxy, Hyy = hessian_matrix(data, sigma=3, order='xy')
+    Hxx, Hxy, Hyy = hessian_matrix(data, sigma=3, order='rc')
     lambda_plus = 0.5 * ((Hxx + Hyy) + np.sqrt((Hxx - Hyy) ** 2 + 4 * Hxy * Hxy))
     lambda_minus = 0.5 * ((Hxx + Hyy) - np.sqrt((Hxx - Hyy) ** 2 + 4 * Hxy * Hxy))
-    theta = 0.5 * np.arctan2(2 * Hxy, Hyy - Hxx) * 180 / np.pi
+    theta = 0.5 * np.arctan2(2 * Hxy, Hxx - Hyy) * 180 / np.pi
     # remove the margins
     lambda_minus = lambda_minus[margin_cut:-margin_cut, margin_cut:-margin_cut]
     lambda_plus = lambda_plus[margin_cut:-margin_cut, margin_cut:-margin_cut]
