fix deltax; change throughput input; remove err computation for spectrograms

spectractor/simulation/simulator.py
@@ -168,7 +168,7 @@ class SpectrumSimulation(Spectrum):
         # Now add the systematics
         if reso > 0.1:
             self.data = fftconvolve_gaussian(self.data, reso)
-            self.err = np.sqrt(np.abs(fftconvolve_gaussian(self.err ** 2, reso)))
+            # self.err = np.sqrt(np.abs(fftconvolve_gaussian(self.err ** 2, reso)))
         if A2 > 0:
             lambdas_binwidths_order2 = np.gradient(lambdas_order2)
             sim_conv = interp1d(lambdas, self.data * lambdas, kind="linear", bounds_error=False, fill_value=(0, 0))

spectractor/simulation/throughput.py
@@ -432,7 +432,7 @@ class TelescopeTransmission:
         wl,trm=ctio.load_mirror_reflectivity(datapath)
         self.tm=interp1d(wl,trm,kind='linear',bounds_error=False,fill_value=0.)
         '''
-        throughput = Throughput()
+        throughput = Throughput(input_directory=parameters.THROUGHPUT_DIR)
         wl, trm, err = throughput.load_total_throughput()
         to = interp1d(wl, trm, kind='linear', bounds_error=False, fill_value=0.)
         err = np.sqrt(err ** 2 + parameters.OBS_TRANSMISSION_SYSTEMATICS ** 2)