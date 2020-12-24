bd9e1db40fd0b56e0f3c7b050605e7029b2d733f
Jérémy Neveu <jneveu@lal.in2p3.fr>
2020-12-24 11:47:50 +0100

    remove fullchain from this script

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -5,7 +5,6 @@ from test_astrometry import *
 from test_simulator import *
 from test_extractor import *
 from test_fitter import *
-from test_fullchain import *
 
 if __name__ == "__main__":
     # Run tests
