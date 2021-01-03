f9c50e3e3503258812271a3505774da2bbaa25c4
Jérémy Neveu <jneveu@lal.in2p3.fr>
2021-01-03 19:22:45 +0100

    test memory usage...

--- tests/test_fullchain.py
+++ tests/test_fullchain.py
@@ -76,7 +76,7 @@ def plot_residuals(spectrum, lambdas_truth, amplitude_truth):
 def make_image():
     spectrum_filename = "./tests/data/reduc_20170530_134_spectrum.fits"
     image_filename = "./tests/data/reduc_20170530_134.fits"
-    ImageSim(image_filename, spectrum_filename, "./tests/data/", A1=A1_T, A2=A2_T,
+    ImageSim(image_filename, spectrum_filename, "./tests/data/", A1=A1_T, A2=0*A2_T,
              psf_poly_params=PSF_POLY_PARAMS_TRUTH, with_stars=True, with_rotation=True)
 
 
