7b427dc851dd502cdd8a561967b2bfd77556ab07
Jérémy Neveu <jneveu@lal.in2p3.fr>
2021-01-12 18:14:22 +0100

    no full image

--- spectractor/simulation/image_simulation.py
+++ spectractor/simulation/image_simulation.py
@@ -442,7 +442,7 @@ def ImageSim(image_filename, spectrum_filename, outputdir, pwv=5, ozone=300, aer
                                            temperature, pwv=pwv, ozone=ozone, aerosols=aerosols, A1=A1, A2=A2,
                                            D=spectrum.disperser.D, shift_x=0., shift_y=0., shift_t=0., B=1.,
                                            psf_poly_params=psf_poly_params, angle=rotation_angle, with_background=False,
-                                           fast_sim=False, full_image=True)
+                                           fast_sim=False, full_image=False)
 
     # now we include effects related to the wrong extraction of the spectrum:
     # wrong estimation of the order 0 position and wrong DISTANCE2CCD
