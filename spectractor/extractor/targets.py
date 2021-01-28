edb6b47d5f88b9223771e20a375fe1c06b13aa5a
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2021-01-27 19:13:17 -0600

    Fix some lookup assumption

--- spectractor/extractor/targets.py
+++ spectractor/extractor/targets.py
@@ -275,7 +275,7 @@ class Star(Target):
                 else:
                     self.wavelengths.append(data.wave)
                     self.spectra.append(data.flux)
-        elif 'HD' in self.label:  # it is a star
+        elif 'HD' in self.label or self.label in parameters.STAR_NAMES:  # it is a star
             self.emission_spectrum = False
             self.hydrogen_only = False
             self.lines = Lines(ATMOSPHERIC_LINES + HYDROGEN_LINES + STELLAR_LINES,
