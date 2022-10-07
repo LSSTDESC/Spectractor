641fe37fbd84ceab575288d0ce9f20d68249860e
Jérémy Neveu <jeremy.neveu@universite-paris-saclay.fr>
2022-10-07 11:09:36 +0200

    clean the code

--- spectractor/extractor/extractor.py
+++ spectractor/extractor/extractor.py
@@ -445,7 +445,7 @@ class FullForwardModelFitWorkspace(FitWorkspace):
             # Evaluate ADR for order 2
             adr_ra, adr_dec = adr_calib(self.lambdas_order2, self.spectrum.adr_params, parameters.OBS_LATITUDE,
                                         lambda_ref=self.spectrum.lambda_ref)
-            adr_x_2, adr_y_2 = flip_and_rotate_adr_to_image_xy_coordinates(adr_ra, adr_dec, dispersion_axis_angle=0)
+            # adr_x_2, adr_y_2 = flip_and_rotate_adr_to_image_xy_coordinates(adr_ra, adr_dec, dispersion_axis_angle=0)
             adr_u_2, adr_v_2 = flip_and_rotate_adr_to_image_xy_coordinates(adr_ra, adr_dec, dispersion_axis_angle=angle)
 
             # Compute lambdas at pixel column x
@@ -461,45 +461,39 @@ class FullForwardModelFitWorkspace(FitWorkspace):
         profile_params[:, 2] += Dy_disp_axis + (0*self.spectrum.spectrogram_y0 + adr_y + dy0) - self.bgd_width
 
         # Prepare order 2 profile params indexed by the wavelength associated to x
-        profile_params_order2 = np.copy(profile_params)
         profile_params_order2 = self.spectrum.chromatic_psf.from_poly_params_to_profile_params(poly_params_order2, apply_bounds=True)
         profile_params_order2[:, 0] = self.spectrum.disperser.ratio_order_2over1(self.lambdas)
-        #profile_params_order2[:, 1] = Dx + self.spectrum.spectrogram_x0 + adr_x_2 + dx0
-        #profile_params_order2[:, 2] = Dy_disp_axis + (self.spectrum.spectrogram_y0 + adr_y_2 + dy0) - self.bgd_width
+        # profile_params_order2[:, 1] = Dx + self.spectrum.spectrogram_x0 + adr_x_2 + dx0
+        # profile_params_order2[:, 2] = Dy_disp_axis + (self.spectrum.spectrogram_y0 + adr_y_2 + dy0) - self.bgd_width
 
         # For each A(lambda)=A_x, affect an order 2 PSF with correct position and
         # same PSF as for the order 1 but at the same position
         distance_order2 = self.spectrum.disperser.grating_lambda_to_pixel(self.lambdas,
                                                                           self.spectrum.x0 + np.asarray([dx0, dy0]),
                                                                           order=self.spectrum.order+np.sign(self.spectrum.order))
-        #print(distance[:5], distance_order2[:5], self.lambdas[:5], self.lambdas_order2[:5])
-        #print("before", profile_params_order2[:5, 1], profile_params_order2[:5, 2], profile_params_order2[:5, 3])
         # for k in range(3, profile_params.shape[1]):
         #     # profile_params_order2[:, k] = interp1d(self.lambdas_order2, profile_params_order2[:, k],
         #     #                                       kind="cubic", fill_value="extrapolate")(self.lambdas)
         #     profile_params_order2[:, k] = interp1d(distance, profile_params_order2[:, k],
         #                                            kind="cubic", fill_value="extrapolate")(distance_order2)
-        #print("after", profile_params_order2[:5, 1], profile_params_order2[:5, 2], profile_params_order2[:5, 3])
         Dx_order2 = distance_order2 * np.cos(angle * np.pi / 180)
         Dy_disp_axis_order2 = distance_order2 * np.sin(angle * np.pi / 180)
         profile_params_order2[:, 1] = Dx_order2 + self.spectrum.spectrogram_x0 + adr_x + dx0
-        #print(Dy_disp_axis[:5], Dy_disp_axis_order2[:5], self.spectrum.spectrogram_y0 + dy0, adr_y, self.bgd_width)
         profile_params_order2[:, 2] += Dy_disp_axis_order2 + (0*self.spectrum.spectrogram_y0 + adr_y + dy0) - self.bgd_width
-        #print("after2", profile_params_order2[:5, 1], profile_params_order2[:5, 2], profile_params_order2[:5, 3])
 
         if parameters.DEBUG and False:
             plt.figure(figsize=(18,4))
             plt.imshow(self.data.reshape((self.Ny, self.Nx)), origin="lower")
             plt.scatter(profile_params[:, 1], profile_params[:, 2], label="profile",
-                         cmap=from_lambda_to_colormap(self.lambdas), c=self.lambdas)
+                        cmap=from_lambda_to_colormap(self.lambdas), c=self.lambdas)
             plt.scatter(profile_params_order2[:, 1], profile_params_order2[:, 2], label="order 2",
-                         cmap=from_lambda_to_colormap(self.lambdas), c=self.lambdas)
+                        cmap=from_lambda_to_colormap(self.lambdas), c=self.lambdas)
             plt.plot(profile_params[:, 1], profile_params[:, 2], label="profile")
             plt.plot(profile_params[:, 1], Dy_disp_axis + self.spectrum.spectrogram_y0 + dy0 - self.bgd_width, 'k-',
-                      label="disp_axis")
+                     label="disp_axis")
             plt.plot(self.spectrum.chromatic_psf.table['Dx'] + self.spectrum.spectrogram_x0 + dx0,
-                      self.spectrum.chromatic_psf.table['Dy'] + self.spectrum.spectrogram_y0 + dy0 - self.bgd_width,
-                      label="y_c")
+                     self.spectrum.chromatic_psf.table['Dy'] + self.spectrum.spectrogram_y0 + dy0 - self.bgd_width,
+                     label="y_c")
             plt.legend()
             plt.title(f"D_CCD={D2CCD:.2f}, dx0={dx0:.2g}, dy0={dy0:.2g}")
             plt.xlim((0, self.Nx))
