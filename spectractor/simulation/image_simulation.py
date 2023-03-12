b8d0cd11fe01c12470608804280fdf2652f999e3
Jérémy Neveu <jeremy.neveu@universite-paris-saclay.fr>
2023-03-12 12:27:37 +0100

    DEBUG: include A1 factor in true_spectrum amplitude

--- spectractor/simulation/image_simulation.py
+++ spectractor/simulation/image_simulation.py
@@ -462,7 +462,7 @@ def ImageSim(image_filename, spectrum_filename, outputdir, pwv=5, ozone=300, aer
     # Recover true spectrum
     spectrogram.set_true_spectrum(spectrogram.lambdas, ozone, pwv, aerosols, shift_t=0)
     true_lambdas = np.copy(spectrogram.true_lambdas)
-    true_spectrum = np.copy(spectrogram.true_spectrum)
+    true_spectrum = A1 * np.copy(spectrogram.true_spectrum)
 
     # Saturation effects
     saturated_pixels = np.where(spectrogram.data > image.saturation)[0]
