2abed1c37ea72a64d7444059b1944ca8816d37c2
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2023-04-10 07:26:59 -0700

    Add drop-in replacement for deprecated make_source_mask function

--- spectractor/extractor/background.py
+++ spectractor/extractor/background.py
@@ -10,8 +10,9 @@ from spectractor.tools import fit_poly1d_outlier_removal, fit_poly2d_outlier_rem
 
 from astropy.stats import SigmaClip
 from photutils import Background2D, SExtractorBackground
-from photutils.segmentation import make_source_mask
+from photutils.segmentation import detect_threshold, detect_sources
 
+from scipy import ndimage
 from scipy.signal import medfilt2d
 from scipy.interpolate import RegularGridInterpolator
 
@@ -41,6 +42,57 @@ def remove_image_background_sextractor(data, sigma=3.0, box_size=(50, 50), filte
         if parameters.PdfPages:
             parameters.PdfPages.savefig()
     return data_wo_bkg
+
+
+def make_source_mask(data, nsigma, npixels, mask=None, sigclip_sigma=3.0,
+                     sigclip_iters=5, dilate_size=11):
+    """
+    Make a source mask using source segmentation and binary dilation.
+
+    This is a slight stripped down version of the method which was removed from
+    photutils in 1.7.0.
+
+    Parameters
+    ----------
+    data : 2D `~numpy.ndarray`
+        The 2D array of the image.
+    nsigma : float
+        The number of standard deviations per pixel above the ``background``
+        for which to consider a pixel as possibly being part of a source.
+    npixels : int
+        The minimum number of connected pixels, each greater than
+        ``threshold``, that an object must have to be detected. ``npixels``
+        must be a positive integer.
+    mask : 2D bool `~numpy.ndarray`, optional
+        A boolean mask with the same shape as ``data``, where a `True` value
+        indicates the corresponding element of ``data`` is masked. Masked
+        pixels are ignored when computing the image background statistics.
+    sigclip_sigma : float, optional
+        The number of standard deviations to use as the clipping limit when
+        calculating the image background statistics.
+    sigclip_iters : int, optional
+        The maximum number of iterations to perform sigma clipping, or `None`
+        to clip until convergence is achieved (i.e., continue until the last
+        iteration clips nothing) when calculating the image background
+        statistics.
+    dilate_size : int, optional
+        The size of the square array used to dilate the segmentation image.
+
+    Returns
+    -------
+    mask : 2D bool `~numpy.ndarray`
+        A 2D boolean image containing the source mask.
+    """
+    sigma_clip = SigmaClip(sigma=sigclip_sigma, maxiters=sigclip_iters)
+    threshold = detect_threshold(data, nsigma, background=None, error=None,
+                                 mask=mask, sigma_clip=sigma_clip)
+
+    segm = detect_sources(data, threshold, npixels)
+    if segm is None:
+        return np.zeros(data.shape, dtype=bool)
+
+    footprint = np.ones((dilate_size, dilate_size))
+    return ndimage.binary_dilation(segm.data.astype(bool), footprint)
 
 
 def extract_spectrogram_background_fit1D(data, err, deg=1, ws=(20, 30), pixel_step=1, sigma=5):
@@ -200,7 +252,7 @@ def extract_spectrogram_background_sextractor(data, err, ws=(20, 30), mask_signa
     Ny, Nx = data.shape
     if Dy_disp_axis is None:
         Dy_disp_axis = np.ones(Nx) * (Ny // 2)
-    
+
     # mask sources
     mask = make_source_mask(data, nsigma=3, npixels=5, dilate_size=11)
     mask += data == 0
