eb5b3d490a140e4c8d977182c5253166adf7f237
Jérémy Neveu <jeremy.neveu@universite-paris-saclay.fr>
2025-04-21 14:34:30 +0200

    calibrate_spectrum with_adr=True if object is a star

--- spectractor/extractor/extractor.py
+++ spectractor/extractor/extractor.py
@@ -1013,7 +1013,7 @@ def run_ffm_minimisation(w, method="newton", niter=2):
                 w.spectrum.err_next_order = np.abs(w.params.values[w.diffraction_orders[1]] * w.amplitude_params_err * w.tr[1](w.lambdas))
 
             # Calibrate the spectrum
-            calibrate_spectrum(w.spectrum, with_adr=True, grid_search=False)
+            calibrate_spectrum(w.spectrum, with_adr=(parameters.OBS_OBJECT_TYPE == "STAR"), grid_search=False)
             w.params.set(r"D_CCD [mm]", w.spectrum.header['D2CCD'])
             w.params.set(r"shift_x [pix]", w.spectrum.header['PIXSHIFT'])
             w.spectrum.convert_from_flam_to_ADUrate()
@@ -1221,10 +1221,7 @@ def SpectractorRun(image, output_directory, guess=None):
 
     # Calibrate the spectrum
     my_logger.info(f'\n\tCalibrating order {spectrum.order:d} spectrum...')
-    with_adr = True
-    if parameters.OBS_OBJECT_TYPE != "STAR":
-        with_adr = False
-    calibrate_spectrum(spectrum, with_adr=with_adr, grid_search=True)
+    calibrate_spectrum(spectrum, with_adr=(parameters.OBS_OBJECT_TYPE == "STAR"), grid_search=True)
     spectrum.data_next_order = np.zeros_like(spectrum.lambdas)
     spectrum.err_next_order = np.zeros_like(spectrum.lambdas)
 
