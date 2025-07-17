f331c7c338d26fdf4c0f069a2fe54b30b3e90351
Jérémy Neveu <jeremy.neveu@ijclab.in2p3.fr>
2025-07-17 07:52:38 -0700

    robuts fit of the polynomial evolution of the individual PSF shape parameters;  chromaticfit1d starts by finding lower order polynomial coefficients and add them gradually

--- spectractor/extractor/chromaticpsf.py
+++ spectractor/extractor/chromaticpsf.py
@@ -1682,8 +1682,11 @@ class ChromaticPSF:
         # keep only brightest transverse profiles
         selected_pixels = self.fitted_pixels & (self.profile_params[:, 0] > 0.1 * np.median(self.profile_params[:, 0]))
         # then keep only profiles with first shape parameter (index=3) are not too deviant from its median value
-        #for k in range(3, self.profile_params.shape[1]):
-        #    selected_pixels = selected_pixels & (np.abs(self.profile_params[:, k]) < 5 * np.median(self.profile_params[:, k]))
+        for k in range(3, self.profile_params.shape[1]):
+            keep = selected_pixels & (np.abs(self.profile_params[:, k]) < 5 * np.median(self.profile_params[:, k]))
+            # keep at least 20% of the fitted pixels or twice the number of data points needed for a polynomial fit
+            if np.sum(keep) > 0.2 * len(selected_pixels) or np.sum(keep) > 2 * (self.deg + 1):
+                selected_pixels = keep
         self.params.values = self.from_profile_params_to_poly_params(self.profile_params, indices=selected_pixels)
         self.from_profile_params_to_shape_params(self.profile_params)
         self.cov_matrix = np.diag(1 / np.array(self.table['flux_err']) ** 2)
@@ -1807,7 +1810,29 @@ class ChromaticPSF:
             w = ChromaticPSFFitWorkspace(self, data, mask=mask, data_errors=data_errors, mode=mode, bgd_model_func=bgd_model_func,
                                         amplitude_priors_method=amplitude_priors_method, verbose=verbose,
                                         live_fit=live_fit, analytical=analytical)
-            run_minimisation(w, method="newton", ftol=1 / (w.Nx * w.Ny), xtol=1e-6, niter=50, with_line_search=True)
+            # first fit order 0 terms
+            w.my_logger.info("\n\tFit order 0 parameters...")
+            fixed_default = np.copy(w.params.fixed)
+            # fix higher order coefficients of polynomes
+            for k in range(w.params.ndim):
+                if "_0" not in w.params.labels[k] and not w.params.fixed[k]:
+                    w.params.fixed[k] = True  # _k parameters that are yet fixed
+                    w.params.values[k] = 0.0  # set them to zero
+            # if zero order coefficient is closer than 5% to its bound, change the value
+            for k, name in enumerate(self.psf.params.labels):
+                if name == 'amplitude':
+                    continue
+                ind = w.params.get_index(name+"_0")
+                val = w.params.values[ind]
+                if (val-self.psf.params.bounds[k][0]) < 0.05 * val:
+                    w.params.values[ind] = min(val * 1.05, self.psf.params.bounds[k][1])
+                if (val-self.psf.params.bounds[k][1]) < 0.05 * val:
+                    w.params.values[ind] = max(val * 0.95, self.psf.params.bounds[k][0])
+            run_minimisation(w, method="newton", ftol=1 / (w.Nx * w.Ny), xtol=1e-6, niter=20, verbose=verbose, with_line_search=False)
+            # then fit all parameters together
+            w.my_logger.info("\n\tFit all ChromaticPSF parameters...")
+            w.params.fixed = fixed_default
+            run_minimisation(w, method="newton", ftol=1 / (w.Nx * w.Ny), xtol=1e-6, niter=50, with_line_search=False)
         elif mode == "2D":
             # first shot to set the mask
             w = ChromaticPSFFitWorkspace(self, data, mask=mask, data_errors=data_errors, mode=mode, bgd_model_func=bgd_model_func,
